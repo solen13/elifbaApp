ec0178c2ea257793728ecf1b032a58d4a742c9c2
solen13 <uusolen13@gmail.com>
2022-12-20 19:55:00 +0300

    20.12.22

---
+++ pages/_lessons/index.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+
+    </div>
+</template>
+
+<script>
+export default {
+  name: "index",
+  mounted() {
+    this.$router.push('/harf-okunus/ustun')
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
